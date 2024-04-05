c64f39ede041ab7785dd93336ffddb3c3ca98637
Samarth Kumar <kumars38@mcmaster.ca>
2024-04-05 00:58:15 -0400

    update messaging and recipient views, themes

--- app/src/main/java/com/example/messagingapp/activities/ChooseRecipientActivity.java
+++ app/src/main/java/com/example/messagingapp/activities/ChooseRecipientActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,7 @@ import java.util.ArrayList;
 public class ChooseRecipientActivity extends AppCompatActivity {
 
     ArrayList<String>  recipientsList = new ArrayList<>();
+    LinearLayout layoutList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,36 +28,28 @@ public class ChooseRecipientActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
 
         setContentView(R.layout.activity_choose_recipient);
+
+        layoutList = findViewById(R.id.recipientsLayout);
+
         this.getRecipientsFromDB();
         this.setRecipientButtons();
     }
 
     private void getRecipientsFromDB() {
-        // *need to get the recipients from account DB
-        // mock data
-        recipientsList.add("A B");
-        recipientsList.add("C D");
-        recipientsList.add("E F");
-        recipientsList.add("G H");
-        // recipientsList.add("I J");
+        // TODO get the actual recipients from our mock account DB
+
+        for (int i=0; i<10; i++) {
+            recipientsList.add("Employee "+i);
+        }
     }
 
-    // can expand for however many people we want for our demo DB
-    // right now its 5
     private void setRecipientButtons() {
-        final int[] buttons = {R.id.recipientButton1, R.id.recipientButton2,
-                R.id.recipientButton3, R.id.recipientButton4, R.id.recipientButton5};
-
-        for (int i=0; i<5; i++) {
-            Button b = findViewById(buttons[i]);
-            if (i >= recipientsList.size()) {
-                b.setVisibility(View.GONE);
-            }
-            else {
-                String r = recipientsList.get(i);
-                Log.d("recipient "+(i+1), r);
-                b.setText(r);
-            }
+        // TODO iterate over User class not Strings
+        for (String recipientName: recipientsList) {
+            View v = getLayoutInflater().inflate(R.layout.recipient,null,false);
+            Button b = v.findViewById(R.id.recipientButton);
+            b.setText(recipientName);
+            layoutList.addView(v);
         }
     }
 
