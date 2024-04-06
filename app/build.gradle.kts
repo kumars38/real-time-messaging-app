e22757399912e3049d706545408c979fafc9efec
Fei <50682656+thaafei@users.noreply.github.com>
2024-04-06 13:12:54 -0400

    Added implementation for firebase messaging
    added to app level gradle to fix import issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     //id("com.android.application")
     id("com.google.gms.google-services")
+
 }
 
 android {
@@ -47,11 +48,12 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
-    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-
+    androidTestImplementation(libs.espresso.core);
+    implementation ("com.google.firebase:firebase-core:16.0.8");
+    implementation ("com.google.firebase:firebase-inappmessaging:17.0.3");
+    implementation(libs.firebase.messaging)
     //Import Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
 }
