ea288347e300183c9b3c0d8c834aebe1bf8f1cb0
Ashwin Unnithan <unnithanash@gmail.com>
2024-04-06 15:13:54 -0400

    Add required client attribute fields in utils

---
+++ app/src/main/java/com/example/messagingapp/utils/client_attributes.java
@@ -0,0 +1,14 @@
+package com.example.messagingapp.utils;
+
+public class client_attributes {
+    public static final String ATTR_COLLECTION = "employees";
+
+    public static final String ATTR_USERID = "userID";
+
+    public static final String ATTR_USERNAME = "username";
+
+    public static final String ATTR_PASSWORD = "password";
+
+    public static final String IS_SIGNED_IN = "IsSignedIn";
+
+}
