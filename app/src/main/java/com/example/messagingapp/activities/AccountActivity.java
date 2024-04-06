6177b4273570d67916d71107d33d315a6e27a1a2
Fei <50682656+thaafei@users.noreply.github.com>
2024-04-05 20:40:19 -0400

    added dropdown for system color

--- app/src/main/java/com/example/messagingapp/activities/AccountActivity.java
+++ app/src/main/java/com/example/messagingapp/activities/AccountActivity.java
@@ -8,6 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import com.example.messagingapp.R;
 
 public class AccountActivity extends AppCompatActivity {
@@ -21,6 +26,34 @@ public class AccountActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        startEventColorSpinnerListener();
+    }
+    private void startEventColorSpinnerListener() {
+        Spinner spinner = findViewById(R.id.sysColorSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.colorsArray,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        // Set an item selected listener to perform actions when an item is selected
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Retrieve the selected item from the spinner
+                String selectedColor = parent.getItemAtPosition(position).toString();
+
+                // Display a toast with the selected color
+                //Toast.makeText(AccountActivity.this, "Selected color: " + selectedColor, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing when nothing is selected
+            }
         });
     }
 
