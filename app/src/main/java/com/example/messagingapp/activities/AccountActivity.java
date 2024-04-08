dcb54938859496f833c105c50f17969c0880f591
Fei <50682656+thaafei@users.noreply.github.com>
2024-04-07 21:18:16 -0400

    updated account activity fontsize choice

--- app/src/main/java/com/example/messagingapp/activities/AccountActivity.java
+++ app/src/main/java/com/example/messagingapp/activities/AccountActivity.java
@@ -2,6 +2,7 @@ package com.example.messagingapp.activities;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,8 @@ import android.widget.Spinner;
 import android.widget.EditText;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
+import android.widget.TextView;
+
 import com.example.messagingapp.R;
 import com.example.messagingapp.models.User;
 import com.example.messagingapp.singleton.MainUser;
@@ -28,28 +31,48 @@ public class AccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         user = MainUser.getInstance().getUserData();
-        updateTheme();
-
+        this.updateTheme();
         setContentView(R.layout.activity_account);
         startEventColorSpinnerListener();
+        startEventTextSpinnerListener();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        /*
-        TODO - Fix LOLOLOLOL
-        prefNameET = findViewById(R.id.prefNameResult);
-        fontSizeET = findViewById(R.id.fontSizeResult);
 
-
-
-        Button updateProfileBtn = findViewById(R.id.updateProfileBtn);
-        updateProfileBtn.setOnClickListener(this::updateProfileBtnClicked);
-        */
+        this.updateFontSize();
 
     }
     private void startEventColorSpinnerListener() {
+        Spinner spinner = findViewById(R.id.sysColorSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.colorsArray,
+                R.layout.spinner_list
+        );
+        adapter.setDropDownViewResource(R.layout.spinner_list);
+        spinner.setAdapter(adapter);
+
+        // Set an item selected listener to perform actions when an item is selected
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Retrieve the selected item from the spinner
+                String selectedColor = parent.getItemAtPosition(position).toString();
+
+                // Display a toast with the selected color
+                //Toast.makeText(AccountActivity.this, "Selected color: " + selectedColor, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing when nothing is selected
+            }
+        });
+    }
+    private void startEventTextSpinnerListener() {
         Spinner spinner = findViewById(R.id.sysColorSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
@@ -83,6 +106,32 @@ public class AccountActivity extends AppCompatActivity {
             case "Green": setTheme(R.style.GreenTheme); break;
             default: setTheme(R.style.BlueTheme);
         }
+    }
+    private void updateFontSize(){
+        float fontSize = 15;
+        switch(user.getProfilePreferences().getFontSize()){
+            case "Small": fontSize = 12; break;
+            case "Medium": fontSize = 15; break;
+            case "Large": fontSize= 18; break;
+        }
+
+        TextView workRole = findViewById(R.id.workRole);
+        TextView workRoleR = findViewById(R.id.workRoleResult);
+        TextView phoneNumber = findViewById(R.id.phoneNumber);
+        TextView phoneNumberR = findViewById(R.id.phoneNumberResult);
+        TextView prefName = findViewById(R.id.prefName);
+        EditText prefNameR = findViewById(R.id.prefNameResult);
+        TextView fSize = findViewById(R.id.fontSize);
+        TextView sysColor = findViewById(R.id.sysColor);
+        
+        workRole.setTextSize(TypedValue.COMPLEX_UNIT_SP,fontSize);
+        workRoleR.setTextSize(TypedValue.COMPLEX_UNIT_SP,fontSize);
+        phoneNumber.setTextSize(TypedValue.COMPLEX_UNIT_SP,fontSize);
+        phoneNumberR.setTextSize(TypedValue.COMPLEX_UNIT_SP,fontSize);
+        prefName.setTextSize(TypedValue.COMPLEX_UNIT_SP,fontSize);
+        prefNameR.setTextSize(TypedValue.COMPLEX_UNIT_SP,fontSize);
+        fSize.setTextSize(TypedValue.COMPLEX_UNIT_SP,fontSize);
+        sysColor.setTextSize(TypedValue.COMPLEX_UNIT_SP,fontSize);
     }
     public void editPressed(View view) {
     }
